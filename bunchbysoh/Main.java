e4374f676f8d5e88e1e14eff2fb08a085422f521
LakshmiJ2002 <97010510+LakshmiJ2002@users.noreply.github.com>
2023-09-12 13:46:57 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -13,32 +13,32 @@ public class Main {                                //Main class containing the m
     static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {    //static method to use methods without creation of objects explisitely of type 'CountsBySoH' 
         CountsBySoH counts = new CountsBySoH();
 
-        for (int capacity : presentCapacities) {    //for-each or enhanced for loop used to test each element of the array named presentCapacities
-            double soh = (capacity * 100.0) / 120; // it calculates SoH as a percentage as givn
+        for (int capacity : presentCapacities) {    
+            double soh = (capacity * 100.0) / 120; // it calculates SoH as a percentage as given
 
-            if (soh > 80) {             // if SoH is greater than 80
+            if (soh > 80) {             // BOUNDARY VALUE TESTING DONE for each domain case
                 counts.healthy++;          //number of healthy batteries is increased
-            } else if (soh >= 63) {     // if SoH is greater than 63
+            } else if (soh >= 63) {     
                 counts.replace++;          //number of replacable batteries is increased
-            } else {                    // if SoH is lesser than 63
-                counts.failed++;            //number of failed batteries iss increased
+            } else {                    
+                counts.failed++;            //number of failed batteries is increased
             }
         }
-        return counts;                  //returns the count of batteries in each domain, when the loop ends
+        return counts;                  
     }
 
   //Second part of the Assignment, where we can decide 
 
-    static void testBucketingByHealth() {      //called from Main method
-        System.out.println("Counting batteries by SoH...\n");      //denotes the main intension of the program to the user
+    static void testBucketingByHealth() {      
+        System.out.println("Counting batteries by SoH...\n");      
         int[] presentCapacities = {115, 118, 80, 95, 91, 72};        //gives the array of capacities of many batteries
-        CountsBySoH counts = countBatteriesByHealth(presentCapacities);    //assigns the returned method values to the variable count
+        CountsBySoH counts = countBatteriesByHealth(presentCapacities);    
 
         System.out.println("Number of Healthy Batteries: " + counts.healthy);        //prints count of each in a easy redeable print statements 
         System.out.println("Number of Replace Batteries: " + counts.replace);
         System.out.println("Number of Failed Batteries: " + counts.failed);
         
-        System.out.println("Done counting :)\n");            //tells the user that it is the end of the program
+        System.out.println("Done counting :)\n");           
     }
 
     public static void main(String[] args) {   //Execution of code starts from here
